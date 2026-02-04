1802fb7bcfce182b317d5c2cbe0aa65efea428e2
Viktor Golovin <rinoally@gmail.com>
2026-02-04 14:47:58 +0300

    add data for day in month and for week

--- src/DatesFormatter.java
+++ src/DatesFormatter.java
@@ -5,10 +5,21 @@ public class DatesFormatter {
     public static void main(String[] args) {
         Date currentDate = new Date();
 
+        SimpleDateFormat currentMonthFormat = new SimpleDateFormat("MMMM");
+        String currentMonth = currentMonthFormat.format(currentDate);
+
+        System.out.println("Current month: " + currentMonth);
+
 
         SimpleDateFormat dayOfWeekFormat = new SimpleDateFormat("EEEE");
         String dayOfWeek = dayOfWeekFormat.format(currentDate);
 
         System.out.println("Current day of the week: " + dayOfWeek);
+
+
+        SimpleDateFormat dayInMonthFormat = new SimpleDateFormat("dd");
+        String dayInMonth = dayInMonthFormat.format(currentDate);
+
+        System.out.println("Current day in month: " + dayInMonth);
     }
 }
