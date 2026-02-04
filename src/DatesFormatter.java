535ce4a1ee6727088fb42067bd9995fb98b61a80
Viktor Golovin <rinoally@gmail.com>
2026-02-04 12:44:37 +0300

    add date pattern

--- src/DatesFormatter.java
+++ src/DatesFormatter.java
@@ -1,2 +1,14 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class DatesFormatter {
+    public static void main(String[] args) {
+        Date currentDate = new Date();
+
+
+        SimpleDateFormat dayOfWeekFormat = new SimpleDateFormat("EEEE");
+        String dayOfWeek = dayOfWeekFormat.format(currentDate);
+
+        System.out.println("Current day of the week: " + dayOfWeek);
+    }
 }
